269810d57f14fd82c2c3481288fe7a53182419d7
GuillaumeRossinot <g.rossinot@gmail.com>
2020-11-13 15:19:05 +0100

    -modification design complet

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ fetch('http://localhost:3000/api/teddies')
         const images1 = document.getElementById("produits");
         for (produit of data) {
             images1.innerHTML += `<div class="col-sm w-100">
-                        <section class="ours">
+                        <section class="produitours">
                             <img src="${produit.imageUrl}" alt="${produit.name}"></br>
                             <p>Nom : <a href="./pages/produit.html?:_id=${produit._id}">${produit.name}</a></p>
                             <p>Description : ${produit.description}<p>
